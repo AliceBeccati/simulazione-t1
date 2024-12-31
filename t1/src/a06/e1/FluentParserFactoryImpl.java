2eeb05a5be94162254c56a68af165e5e2505335b
Alice Beccati <alice.beccati@studio.unibo.it>
2024-12-31 05:03:28 +0100

    fine

--- t1/src/a06/e1/FluentParserFactoryImpl.java
+++ t1/src/a06/e1/FluentParserFactoryImpl.java
@@ -89,28 +89,6 @@ public class FluentParserFactoryImpl implements FluentParserFactory{
 
         };
     }
-/* 
-    @Override
-    public FluentParser<Pair<Integer, List<String>>> incrementalPairs(int i0, UnaryOperator<Integer> op, String s) {
-        return new FluentParser<Pair<Integer, List<String>>>(){
-            private int i = 0;
-            private FluentParser<Integer> parserNX = naturals();
-            private FluentParser<Integer> parserNY = naturals();
-
-            @Override
-            public FluentParser<Pair<Integer, List<String>>> accept(Pair<Integer, List<String>> value) {
-                try {
-                    parserNX.accept(value.getX());
-                    parserNY.accept(value.getY().size());
-                } catch (IllegalStateException e) {
-                    if (i >= i0 && i){
-
-                    }
-                }
-                return this;
-            }
-
-        };*/
 
     @Override
     public FluentParser<Pair<Integer, List<String>>> incrementalPairs(int i0, UnaryOperator<Integer> op, String s) {
